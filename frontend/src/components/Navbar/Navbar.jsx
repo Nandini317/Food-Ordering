ea9f14ea763b93a9d6b2ea76f776b8b098e3f821
Nandini <154619218+Nandini317@users.noreply.github.com>
2026-01-01 13:51:59 +0530

    removing  menu link

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -29,13 +29,7 @@ const Navbar = ({ setShowLogin }) => {
         >
           Home
         </Link>
-        {/* <a
-          href="#explore-menu"
-          onClick={() => setMenu("menu")}
-          className={menu === "menu" ? "active" : ""}
-        >
-          menu
-        </a> */}
+        
         <Link to="/myorders" 
         onClick={() => setMenu("orders")}
           className={menu === "orders" ? "active" : ""}
